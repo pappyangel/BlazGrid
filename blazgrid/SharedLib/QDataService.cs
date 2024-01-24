9d35b44312cadc726b550e9268cb80df26f69304
Jim Vacca <33520581+MSFTJim@users.noreply.github.com>
2024-01-24 01:11:19 +0000

    Add SharedLib project and QDataService class

---
+++ blazgrid/SharedLib/QDataService.cs
@@ -0,0 +1,23 @@
+﻿﻿namespace SharedLib;
+
+public class QDataService
+{
+    public string GiveRandomNumber()
+    {
+        int randomValue = Random.Shared.Next(1,100);
+        return $"The value is {randomValue}";
+    }
+
+    public IQueryable<PersonClass>? GetPeople(int rowCount, int SeedKey)
+    {
+        var testUsers = new Bogus.Faker<PersonClass>()
+            .RuleFor(u => u.Id, f => ++SeedKey)
+            .RuleFor(u => u.FirstName, f => f.Name.FirstName())
+            .RuleFor(u => u.LastName, f => f.Name.LastName())
+            .RuleFor(u => u.StartDate, f => f.Date.Past(22, DateTime.Now))
+            .Generate(rowCount);
+        
+        return testUsers.AsQueryable();
+    }
+
+}
--- blazgrid/blazgrid.Client/Program.cs
+++ blazgrid/blazgrid.Client/Program.cs
@@ -1,5 +1,9 @@
+using SharedLib;
+
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+builder.Services.AddSingleton<QDataService>();
 
 await builder.Build().RunAsync();
